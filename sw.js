9d94a5186d05c61618e2253b6f3a66f234fcde13
Federico <111776188+FedericoG94@users.noreply.github.com>
2026-08-21 00:35:13 +0200

    Invalidate stale iOS cache after compatibility fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='geometry-dash-v8';
+const CACHE='geometry-dash-v9';
 const ASSETS=['./','./index.html','./app.js','./levels.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
