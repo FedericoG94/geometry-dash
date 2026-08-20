a59b340fa6ff4f9d8099e4dbd1260abe951ab8f9
Federico <111776188+FedericoG94@users.noreply.github.com>
2026-08-21 00:14:25 +0200

    Harden service worker and remove unsupported request caching

--- sw.js
+++ sw.js
@@ -1,17 +1,5 @@
 const CACHE='geometry-dash-v3';
-const ASSETS=['./','./index.html','./app.js','./app-fixed.js','./levels.js','./manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{
-  const request=event.request;
-  if(request.method!=='GET') return;
-  const url=new URL(request.url);
-  if(url.protocol!=='http:'&&url.protocol!=='https:') return;
-  event.respondWith(caches.match(request).then(cached=>cached||fetch(request).then(response=>{
-    if(response.ok && (url.origin===self.location.origin || request.destination==='document')){
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(request,copy)).catch(()=>{});
-    }
-    return response;
-  }).catch(()=>caches.match('./index.html'))));
-});
+const ASSETS=['./','./index.html','./app.js','./levels.js','./manifest.webmanifest','./favicon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{const request=event.request;if(request.method!=='GET')return;let url;try{url=new URL(request.url)}catch{return}if(url.protocol!=='http:'&&url.protocol!=='https:')return;event.respondWith(caches.match(request).then(cached=>{if(cached)return cached;return fetch(request).then(response=>{if(response&&response.ok&&(response.type==='basic'||response.type==='cors')){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(request,copy)).catch(()=>{})}return response}).catch(()=>caches.match('./index.html'))}))});
