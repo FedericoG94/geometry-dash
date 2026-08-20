45bf1fee08219daa26df72156928cc37532a2ab9
Federico <111776188+FedericoG94@users.noreply.github.com>
2026-08-21 00:11:50 +0200

    Fix service worker cross-origin request caching and offline caching

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
-const CACHE='geometry-dash-v2';const ASSETS=['./','./index.html','./app.js','./levels.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
+const CACHE='geometry-dash-v3';
+const ASSETS=['./','./index.html','./app.js','./app-fixed.js','./levels.js','./manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  const request=event.request;
+  if(request.method!=='GET') return;
+  const url=new URL(request.url);
+  if(url.protocol!=='http:'&&url.protocol!=='https:') return;
+  event.respondWith(caches.match(request).then(cached=>cached||fetch(request).then(response=>{
+    if(response.ok && (url.origin===self.location.origin || request.destination==='document')){
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(request,copy)).catch(()=>{});
+    }
+    return response;
+  }).catch(()=>caches.match('./index.html'))));
+});
