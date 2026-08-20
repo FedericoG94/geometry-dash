5b9bccf12665888267db9757960bf79c2c391caf
Federico <111776188+FedericoG94@users.noreply.github.com>
2026-08-21 00:48:13 +0200

    Use corrected app in offline cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='geometry-dash-v11';
-const ASSETS=['./','./index.html','./app.js?v=11','./levels.js','./manifest.webmanifest'];
+const CACHE='geometry-dash-v12';
+const ASSETS=['./','./index.html','./app.js?v=10','./levels.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
