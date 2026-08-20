efc9c883783763e4df2e0df32c67fdb535bd299f
Federico <111776188+FedericoG94@users.noreply.github.com>
2026-08-21 00:38:58 +0200

    Bump service worker cache for gameplay fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='geometry-dash-v9';
-const ASSETS=['./','./index.html','./app.js','./levels.js','./manifest.webmanifest'];
+const CACHE='geometry-dash-v10';
+const ASSETS=['./','./index.html','./app.js?v=10','./levels.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
@@ -10,9 +10,7 @@ self.addEventListener('fetch',event=>{
  event.respondWith(caches.match(req).then(cached=>{
   if(cached)return cached;
   return fetch(req).then(res=>{
-   if(res&&res.ok&&(res.type==='basic'||res.type==='cors')){
-    const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});
-   }
+   if(res&&res.ok&&(res.type==='basic'||res.type==='cors')){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});}
    return res;
   }).catch(()=>caches.match('./index.html'));
  }));
